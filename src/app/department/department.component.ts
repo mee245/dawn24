89c162f9cf0508509d93e70fa8fedfd54bda8283
Meenal Saini <meenalsaini245@gmail.com>
2020-03-11 13:32:51 +0530

    angular

--- src/app/department/department.component.ts
+++ src/app/department/department.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../service/user.service';
 import {Posts} from '../classes/posts'
+// import { HttpClient } from '@angular/common/http';
 
 
 @Component({
